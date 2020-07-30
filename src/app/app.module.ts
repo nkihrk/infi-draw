6ea006c7b0d88b8f09ee16ff9d5924a33e73d69c
nkihrk <shoma.hamada@gmail.com>
2020-07-30 15:46:55 +0900

    added dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CanvasComponent } from './canvas/canvas.component';
 import { EventDirective } from './shared/directive/event.directive';
+import { ToolsComponent } from './tools/tools.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
-  declarations: [AppComponent, CanvasComponent, EventDirective],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, CanvasComponent, EventDirective, ToolsComponent, DashboardComponent, MenuComponent],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
