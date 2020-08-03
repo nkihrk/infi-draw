02fa5b54cae513a387500455fbceed2d163a1d59
nkihrk <40584391+nkihrk@users.noreply.github.com>
2020-08-03 09:00:31 +0900

    added brush-size

--- src/app/tool-bar/tool-bar.component.ts
+++ src/app/tool-bar/tool-bar.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { faHandPaper } from '@fortawesome/free-regular-svg-icons';
+import { faMousePointer } from '@fortawesome/free-solid-svg-icons';
 import { faPenNib } from '@fortawesome/free-solid-svg-icons';
 import { faEraser } from '@fortawesome/free-solid-svg-icons';
 import { faSquare } from '@fortawesome/free-regular-svg-icons';
 import { faCircle } from '@fortawesome/free-regular-svg-icons';
 import { faFont } from '@fortawesome/free-solid-svg-icons';
+import { faEyeDropper } from '@fortawesome/free-solid-svg-icons';
+import { faSearchPlus } from '@fortawesome/free-solid-svg-icons';
+import { faSearchMinus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-tool-bar',
@@ -12,12 +16,16 @@ import { faFont } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./tool-bar.component.scss']
 })
 export class ToolBarComponent implements OnInit {
+  faMousePointer = faMousePointer;
   faHandPaper = faHandPaper;
   faPenNib = faPenNib;
   faEraser = faEraser;
   faSquare = faSquare;
   faCircle = faCircle;
   faFont = faFont;
+  faEyeDropper = faEyeDropper;
+  faSearchPlus = faSearchPlus;
+  faSearchMinus = faSearchMinus;
 
   constructor() { }
 
