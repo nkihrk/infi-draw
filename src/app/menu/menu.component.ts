7a5b9dde202efd95a232f68e6a2be1850b8836c7
nkihrk <40584391+nkihrk@users.noreply.github.com>
2020-08-03 10:53:03 +0900

    added account and app name to menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faPenNib } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-menu',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+  faPenNib = faPenNib;
+  faUser = faUser;
 
   constructor() { }
 
