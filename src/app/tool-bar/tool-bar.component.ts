f9183ca5fd408dc78a1575ce4145cde3bdb5595b
nkihrk <shoma.hamada@gmail.com>
2020-08-11 08:02:53 +0900

    Implemented tool-bar sign

--- src/app/tool-bar/tool-bar.component.ts
+++ src/app/tool-bar/tool-bar.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { MemoryService } from '../shared/service/core/memory.service';
+import { FuncService } from '../shared/service/core/func.service';
+
+// Fontawesome
 import { faHandPaper } from '@fortawesome/free-regular-svg-icons';
 import { faMousePointer } from '@fortawesome/free-solid-svg-icons';
 import { faPenNib } from '@fortawesome/free-solid-svg-icons';
@@ -16,6 +20,18 @@ import { faSearchMinus } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./tool-bar.component.scss']
 })
 export class ToolBarComponent implements OnInit {
+  @ViewChild('select', {static: true}) select: ElementRef<HTMLDivElement>;
+  @ViewChild('hand', {static: true}) hand: ElementRef<HTMLDivElement>;
+  @ViewChild('draw', {static: true}) draw: ElementRef<HTMLDivElement>;
+  @ViewChild('erase', {static: true}) erase: ElementRef<HTMLDivElement>;
+  @ViewChild('createSquare', {static: true}) createSquare: ElementRef<HTMLDivElement>;
+  @ViewChild('createCircle', {static: true}) createCircle: ElementRef<HTMLDivElement>;
+  @ViewChild('createLine', {static: true}) createLine: ElementRef<HTMLDivElement>;
+  @ViewChild('font', {static: true}) font: ElementRef<HTMLDivElement>;
+  @ViewChild('colorPicker', {static: true}) colorPicker: ElementRef<HTMLDivElement>;
+  @ViewChild('zoomIn', {static: true}) zoomIn: ElementRef<HTMLDivElement>;
+  @ViewChild('zoomOut', {static: true}) zoomOut: ElementRef<HTMLDivElement>;
+
   faMousePointer = faMousePointer;
   faHandPaper = faHandPaper;
   faPenNib = faPenNib;
@@ -27,9 +43,128 @@ export class ToolBarComponent implements OnInit {
   faSearchPlus = faSearchPlus;
   faSearchMinus = faSearchMinus;
 
-  constructor() { }
+  constructor(private memory: MemoryService, private func: FuncService) { }
 
   ngOnInit(): void {
+    this.render();
   }
 
+  render(): void {
+    const r: FrameRequestCallback = () => {
+      this._render();
+
+      requestAnimationFrame(r);
+    };
+    requestAnimationFrame(r);
+  }
+
+  _render(): void {
+    const name: string = this.memory.reservedByFunc.name;
+    let t: HTMLDivElement;
+
+    switch (name) {
+      case 'select':
+        t = this.select.nativeElement;
+        if (!t.classList.contains('active')) {
+          this._resetToolBarClassAll();
+        }
+        break;
+
+      case 'hand':
+        t = this.hand.nativeElement;
+        if (!t.classList.contains('active')) {
+          this._resetToolBarClassAll();
+          t.classList.add('active');
+        }
+        break;
+
+      case 'draw':
+        t = this.draw.nativeElement;
+        if (!t.classList.contains('active')) {
+          this._resetToolBarClassAll();
+          t.classList.add('active');
+        }
+        break;
+
+      case 'erase':
+        t = this.erase.nativeElement;
+        if (!t.classList.contains('active')) {
+          this._resetToolBarClassAll();
+          t.classList.add('active');
+        }
+        break;
+
+      case 'createSquare':
+        t = this.createSquare.nativeElement;
+        if (!t.classList.contains('active')) {
+          this._resetToolBarClassAll();
+        }
+        break;
+
+      case 'createCircle':
+        t = this.createCircle.nativeElement;
+        if (!t.classList.contains('active')) {
+          this._resetToolBarClassAll();
+        }
+        break;
+
+      case 'createLine':
+        t = this.createLine.nativeElement;
+        if (!t.classList.contains('active')) {
+          this._resetToolBarClassAll();
+        }
+        break;
+
+      case 'font':
+        t = this.font.nativeElement;
+        if (!t.classList.contains('active')) {
+          this._resetToolBarClassAll();
+        }
+        break;
+
+      case 'colorPicker':
+        t = this.colorPicker.nativeElement;
+        if (!t.classList.contains('active')) {
+          this._resetToolBarClassAll();
+        }
+        break;
+
+      case 'zoomIn':
+        t = this.zoomIn.nativeElement;
+        if (!t.classList.contains('active')) {
+          this._resetToolBarClassAll();
+        }
+        break;
+
+      case 'zoomOut':
+        t = this.zoomOut.nativeElement;
+        if (!t.classList.contains('active')) {
+          this._resetToolBarClassAll();
+        }
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  private _resetToolBarClassAll(): void {
+    this._resetToolBarClass(this.select.nativeElement);
+    this._resetToolBarClass(this.hand.nativeElement);
+    this._resetToolBarClass(this.draw.nativeElement);
+    this._resetToolBarClass(this.erase.nativeElement);
+    this._resetToolBarClass(this.createSquare.nativeElement);
+    this._resetToolBarClass(this.createCircle.nativeElement);
+    this._resetToolBarClass(this.createLine.nativeElement);
+    this._resetToolBarClass(this.font.nativeElement);
+    this._resetToolBarClass(this.colorPicker.nativeElement);
+    this._resetToolBarClass(this.zoomIn.nativeElement);
+    this._resetToolBarClass(this.zoomOut.nativeElement);
+  }
+
+  private _resetToolBarClass($targetElem: HTMLDivElement): void {
+    $targetElem.className = '';
+    $targetElem.classList.add('icon-prefix');
+    $targetElem.classList.add('name-info-wrapper');
+  }
 }
