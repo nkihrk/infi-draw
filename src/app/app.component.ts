e1b95432c3654b61fc79369aab5a4bef7f76dba1
nkihrk <40584391+nkihrk@users.noreply.github.com>
2020-08-12 05:21:41 +0900

    Refactored codes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss', './_reset.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {}
