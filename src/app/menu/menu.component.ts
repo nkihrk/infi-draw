2a6ed517fb958c3fe50474e20ddd7df1202c3d35
nkihrk <shoma.hamada@gmail.com>
2020-08-12 06:42:28 +0900

    Added toggle active method for menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { faPenNib } from '@fortawesome/free-solid-svg-icons';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
@@ -9,12 +9,28 @@ import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+  @ViewChild('file', {static: true}) file: ElementRef<HTMLDivElement>;
+  @ViewChild('edit', {static: true}) edit: ElementRef<HTMLDivElement>;
+  @ViewChild('modify', {static: true}) modify: ElementRef<HTMLDivElement>;
+  @ViewChild('show', {static: true}) show: ElementRef<HTMLDivElement>;
+  @ViewChild('help', {static: true}) help: ElementRef<HTMLDivElement>;
+
   faPenNib = faPenNib;
   faUser = faUser;
   faCaretRight = faCaretRight;
 
-  constructor() { }
-
   ngOnInit(): void {
+  }
+
+  toggleActiveMenu(): void {
+    this._addActiveToMenu(this.file.nativeElement);
+    this._addActiveToMenu(this.edit.nativeElement);
+    this._addActiveToMenu(this.modify.nativeElement);
+    this._addActiveToMenu(this.show.nativeElement);
+    this._addActiveToMenu(this.help.nativeElement);
+  }
+
+  private _addActiveToMenu($target: HTMLDivElement): void {
+    if (!$target.classList.contains('active')) $target.classList.add('active');
   }
 }
