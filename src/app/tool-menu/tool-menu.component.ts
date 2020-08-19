e12c8cb85eafd0954ad6d0ee68107f6e4fed9f8d
nkihrk <40584391+nkihrk@users.noreply.github.com>
2020-08-19 18:33:37 +0900

    Successfuly implemented pickr. Added patches for pickr styles

--- src/app/tool-menu/tool-menu.component.ts
+++ src/app/tool-menu/tool-menu.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { MemoryService } from '../shared/service/core/memory.service';
 import { FuncService } from '../shared/service/core/func.service';
 
+import Pickr from '@simonwep/pickr/dist/pickr.es5.min';
+
 // Fontawesome
 import { faSave } from '@fortawesome/free-regular-svg-icons';
 import { faUndo } from '@fortawesome/free-solid-svg-icons';
@@ -34,7 +36,58 @@ export class ToolMenuComponent implements OnInit {
 		// Initialize brushSizeSlider
 		this.memory.initBrushSizeSlider(this.brushSizeWrapperRef, this.brushSizeMeterRef);
 
+		// Create pickr
+		this._createPickr();
+
+		// Check current states for tool-menu
 		this.render();
+	}
+
+	private _createPickr(): void {
+		const pickr = Pickr.create({
+			el: '#pickr',
+			container: '#pickr-wrapper',
+			theme: 'monolith',
+			appClass: 'custom-pickr',
+			padding: 20,
+			autoReposition: true,
+
+			swatches: [
+				'rgba(244, 67, 54, 1)',
+				'rgba(233, 30, 99, 0.95)',
+				'rgba(156, 39, 176, 0.9)',
+				'rgba(103, 58, 183, 0.85)',
+				'rgba(63, 81, 181, 0.8)',
+				'rgba(33, 150, 243, 0.75)',
+				'rgba(3, 169, 244, 0.7)',
+				'rgba(0, 188, 212, 0.7)',
+				'rgba(0, 150, 136, 0.75)',
+				'rgba(76, 175, 80, 0.8)',
+				'rgba(139, 195, 74, 0.85)',
+				'rgba(205, 220, 57, 0.9)',
+				'rgba(255, 235, 59, 0.95)',
+				'rgba(255, 193, 7, 1)'
+			],
+
+			components: {
+				// Main components
+				preview: true,
+				opacity: true,
+				hue: true,
+
+				// Input / output Options
+				interaction: {
+					hex: false,
+					rgba: false,
+					hsla: false,
+					hsva: false,
+					cmyk: false,
+					input: false,
+					clear: false,
+					save: true
+				}
+			}
+		});
 	}
 
 	render(): void {
