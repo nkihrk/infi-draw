14373363e6fa82e45e9f61b3e0d7b9d32b4479ea
nkihrk <shoma.hamada@gmail.com>
2020-08-24 06:29:42 +0900

    Refactored codes to add new points automatically

--- src/app/shared/service/module/pen.service.ts
+++ src/app/shared/service/module/pen.service.ts
@@ -20,8 +20,53 @@ export class PenService {
 	recordTrail(): void {
 		const trailId: number = this.memory.trailList.length > 0 ? this.memory.trailList.length - 1 : 0;
 		const trail: Trail = this.memory.trailList[trailId];
+		const point: Point = this._creatPoint(trail);
+
+		this.validateMinMax(trail, point.offset.newOffsetX, point.offset.newOffsetY);
+		if (trail.points.length > 0) {
+			this.addNewPoints(trail, point);
+		} else {
+			trail.points.push(point);
+		}
+	}
+
+	private addNewPoints($trail: Trail, $currentP: Point): void {
+		const prevP: Point = $trail.points[$trail.points.length - 1];
+		const dist: number = this._distanceBetween(prevP, $currentP);
+		const angle: number = this._angleBetween(prevP, $currentP);
+
+		for (let i = 0; i < dist; i++) {
+			const x: number = prevP.offset.newOffsetX + Math.sin(angle) * i;
+			const y: number = prevP.offset.newOffsetY + Math.cos(angle) * i;
+			const point: Point = this._creatPoint($trail);
+			point.offset = {
+				newOffsetX: x,
+				newOffsetY: y,
+				prevOffsetX: x,
+				prevOffsetY: y
+			};
+
+			$trail.points.push(point);
+		}
+	}
+
+	private _distanceBetween($prevP: Point, $currentP: Point): number {
+		return Math.sqrt(
+			Math.pow($currentP.offset.newOffsetX - $prevP.offset.newOffsetX, 2) +
+				Math.pow($currentP.offset.newOffsetY - $prevP.offset.newOffsetY, 2)
+		);
+	}
+
+	private _angleBetween($prevP: Point, $currentP: Point): number {
+		return Math.atan2(
+			$currentP.offset.newOffsetX - $prevP.offset.newOffsetX,
+			$currentP.offset.newOffsetY - $prevP.offset.newOffsetY
+		);
+	}
+
+	private _creatPoint($trail: Trail): Point {
 		const point: Point = {
-			id: trail.points.length,
+			id: $trail.points.length,
 			color: this.memory.brush.color,
 			visibility: true,
 			offset: {
@@ -34,22 +79,10 @@ export class PenService {
 			lineWidth: this.memory.brush.lineWidth.draw
 		};
 
-		if (this._ignoreDuplication(point.offset.prevOffsetX, point.offset.prevOffsetY)) {
-			this._validateMinMax(trail, point.offset.newOffsetX, point.offset.newOffsetY);
-			trail.points.push(point);
-			console.log(trail.points.length);
-		}
+		return point;
 	}
 
-	private _validateMinMax($trail: Trail, $x: number, $y: number): void {
-		$trail.min.newOffsetX = Math.min($trail.min.newOffsetX, $x);
-		$trail.min.newOffsetY = Math.min($trail.min.newOffsetY, $y);
-
-		$trail.max.newOffsetX = Math.max($trail.max.newOffsetX, $x);
-		$trail.max.newOffsetY = Math.max($trail.max.newOffsetY, $y);
-	}
-
-	private _ignoreDuplication($x: number, $y: number): boolean {
+	private ignoreDuplication($x: number, $y: number): boolean {
 		const trailId: number = this.memory.trailList.length > 0 ? this.memory.trailList.length - 1 : 0;
 		const trail: Trail = this.memory.trailList[trailId];
 		const points: Point[] = trail.points;
@@ -64,5 +97,13 @@ export class PenService {
 		}
 
 		return true;
+	}
+
+	private validateMinMax($trail: Trail, $x: number, $y: number): void {
+		$trail.min.newOffsetX = Math.min($trail.min.newOffsetX, $x);
+		$trail.min.newOffsetY = Math.min($trail.min.newOffsetY, $y);
+
+		$trail.max.newOffsetX = Math.max($trail.max.newOffsetX, $x);
+		$trail.max.newOffsetY = Math.max($trail.max.newOffsetY, $y);
 	}
 }
